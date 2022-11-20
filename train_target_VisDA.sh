a5bf3edf977234cf3868321374b3f40c7f1dc973
anonymous2023csfda <118587050+anonymous2023csfda@users.noreply.github.com>
2022-11-20 00:15:01 -0500

    Update train_target_VisDA.sh

--- train_target_VisDA.sh
+++ train_target_VisDA.sh
@@ -1,4 +1,4 @@
-SRC_MODEL_DIR="../checkpoint/visda_source/"        ## Put the source model here
+SRC_MODEL_DIR="../checkpoint/"        ## Put the source model here
 
 PORT=10001
 MEMO="target"
@@ -7,7 +7,7 @@ for SEED in 2021 2022
 do
     python main_csfda.py \
     seed=${SEED} port=${PORT} memo=${MEMO} project="VISDA-C" \
-    data.data_root="${PWD}/../data/" data.workers=16 \
+    data.data_root="${PWD}/data/" data.workers=16 \
     data.dataset="VISDA-C" data.source_domains="[train]" data.target_domains="[validation]" \
     model_src.arch="resnet101" \
     model_tta.src_log_dir=${SRC_MODEL_DIR} \
