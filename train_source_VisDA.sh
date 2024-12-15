6238687118eed0fc58df26618c92e81329ea7dbd
Md Nazmul Karim <nazmul.karim170@gmail.com>
2024-12-15 00:12:00 -0500

    Update train_source_VisDA.sh

--- train_source_VisDA.sh
+++ train_source_VisDA.sh
@@ -3,7 +3,7 @@ MEMO="source"
 
 for SEED in 2020 2021 2022
 do
-    python main_adacontrast.py train_source=true learn=source \
+    python main_csfda.py train_source=true learn=source \
     seed=${SEED} port=${PORT} memo=${MEMO} project="VISDA-C" \
     data.data_root="${PWD}/datasets" data.workers=8 \
     data.dataset="VISDA-C" data.source_domains="[train]" data.target_domains="[validation]" \
